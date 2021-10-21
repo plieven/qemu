0eb3a7aeb7d88349205255f5adb74d8efd86e339
Jon Maloy <jmaloy@redhat.com>
2021-10-21 12:10:47 -0400

    e1000: fix tx re-entrancy problem
    The fact that the MMIO handler is not re-entrant causes an infinite
    loop under certain conditions:
    
    Guest write to TDT ->  Loopback -> RX (DMA to TDT) -> TX
    
    We now eliminate the effect of this problem locally in e1000, by adding
    a boolean in struct E1000State indicating when the TX side is busy. This
    will cause any entering new call to return early instead of interfering
    with the ongoing work, and eliminates any risk of looping.
    
    This is intended to address CVE-2021-20257.
    
    Signed-off-by: Jon Maloy <jmaloy@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- hw/net/e1000.c
+++ hw/net/e1000.c
@@ -103,6 +103,7 @@ typedef struct E1000State_st {
         e1000x_txd_props props;
         e1000x_txd_props tso_props;
         uint16_t tso_frames;
+        bool busy;
     } tx;
 
     struct {
@@ -743,6 +744,11 @@ start_xmit(E1000State *s)
         return;
     }
 
+    if (s->tx.busy) {
+        return;
+    }
+    s->tx.busy = true;
+
     while (s->mac_reg[TDH] != s->mac_reg[TDT]) {
         base = tx_desc_base(s) +
                sizeof(struct e1000_tx_desc) * s->mac_reg[TDH];
@@ -769,6 +775,7 @@ start_xmit(E1000State *s)
             break;
         }
     }
+    s->tx.busy = false;
     set_ics(s, 0, cause);
 }
 
